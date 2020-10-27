d53f298776d556eca5e9de80ca7869fed8e33afe
Feldwor <BoQsc@users.noreply.github.com>
2020-10-27 22:06:39 +0200

    Update exception.cmd

--- automated/exception.cmd
+++ automated/exception.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-
-netsh advfirewall firewall add rule name="My Applicationhhook" dir=in action=allow program="C:\Windows\System32\SppExtComObjHook.dll" enable=yes
 netsh advfirewall firewall delete rule name="My Applicationhhook"
+netsh advfirewall firewall add rule name="My Applicationhhook" dir=in action=allow program="C:\Windows\System32\SppExtComObjHook.dll" enable=yes
+
 pause
