88bb23d476842bab2a3840891facfb9e9c4623b3
Feldwor <BoQsc@users.noreply.github.com>
2021-07-07 13:10:20 +0300

    Add files via upload

--- automated/2021/automated.cmd
+++ automated/2021/automated.cmd
@@ -11,29 +11,50 @@ EXIT
 CD "%~dp0"
 ECHO %cd%
 IF EXIST "C:\Windows\System32\curl.exe" (
-	curl -L -O https://github.com/BoQsc/microsoft-office-2016/raw/BoQsc-patch-1/O2016RTool.zip
+	curl -L -O https://github.com/BoQsc/microsoft-office-2016/raw/BoQsc-patch-1/automated/2021/O2016RToolModified.zip
+			powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%CD%\O2016RToolModified.zip'"
 ) ELSE (
 	IF EXIST "C:\Windows\System32\bitsadmin.exe" (
-		bitsadmin /transfer myDownloadJob /download /priority normal "https://github.com/BoQsc/microsoft-office-2016/raw/BoQsc-patch-1/O2016RTool.zip" "%CD%O2016RTool.zip"
+		bitsadmin /transfer myDownloadJob /download /priority normal "https://github.com/BoQsc/microsoft-office-2016/raw/BoQsc-patch-1/automated/2021/O2016RToolModified.zip" "%CD%O2016RToolModified.zip"
+		powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%CD%\O2016RToolModified.zip'"
 	) ELSE (
-
 		CD "%USERPROFILE%\Downloads"
-		powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%CD%\0O2016RTool.zip'"
-		IF EXIST "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" START /MIN /WAIT "" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"  --incognito https://github.com/BoQsc/microsoft-office-2016/raw/BoQsc-patch-1/O2016RTool.zip"
-		IF EXIST "%ProgramFiles%\Google\Chrome\Application\chrome.exe" START /MIN /WAIT "" "%ProgramFiles%\Google\Chrome\Application\chrome.exe"  --incognito https://github.com/BoQsc/microsoft-office-2016/raw/BoQsc-patch-1/O2016RTool.zip"
-		
+		powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%CD%\O2016RToolModified.zip'"
+		IF EXIST "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" START /MIN /WAIT "" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"  --incognito https://github.com/BoQsc/microsoft-office-2016/raw/BoQsc-patch-1/automated/2021/O2016RToolModified.zip"
+		IF EXIST "%ProgramFiles%\Google\Chrome\Application\chrome.exe" START /MIN /WAIT "" "%ProgramFiles%\Google\Chrome\Application\chrome.exe"  --incognito https://github.com/BoQsc/microsoft-office-2016/raw/BoQsc-patch-1/automated/2021/O2016RToolModified.zip"
+		1GB.zip.crdownload
 		
 	)
 )
-MKDIR ".\0O2016RTool"
-powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%CD%\0O2016RTool'"
-tar -xf ".\O2016RTool.zip" --directory ".\0O2016RTool"
-@ECHO ON
-START /WAIT "" ".\0O2016RTool\O2016RTool.cmd" | BREAK 
 
-RD /S /Q ".\0O2016RTool"
-DEL ".\O2016RTool.zip"
-powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '%CD%\0O2016RTool'"
-powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '%CD%\0O2016RTool.zip'"
+
+
+
+
+MKDIR "%cd%\O2016RToolModified"
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%CD%\O2016RToolModified'"
+tar -xf %cd%\O2016RToolModified.zip --directory %cd%\
+START /WAIT "" ".\O2016RToolModified\O2016RTool.cmd" | BREAK 
+
+RD /S /Q ".\O2016RToolModified"
+DEL ".\O2016RToolModified.zip"
+powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '%CD%\O2016RToolModified'"
+powershell -inputformat none -outputformat none -NonInteractive -Command "Remove-MpPreference -ExclusionPath '%CD%\O2016RToolModified.zip'"
 
 REM DEL test.cmd
+
+
+
+
+
+
+:waitChromeDownload
+
+IF EXIST "%USERPROFILE%\Downloads\O2016RToolModified.zip.crdownload" (
+	ECHO O2016RToolModified.zip has been incompleted by Chrome.
+	ECHO Please wait until the download is completed.
+	timeout /t 3
+	GOTO :waitChromeDownload
+)
+
+GOTO :EOF
